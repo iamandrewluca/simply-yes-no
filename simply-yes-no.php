fed6c3a2b0af7dd0f337afea36c961a0287e9062
Andrew Luca <thendrluca@gmail.com>
2019-02-28 00:05:55 +0200

    feat: add required acf

--- simply-yes-no.php
+++ simply-yes-no.php
@@ -9,7 +9,6 @@
  **/
 
 declare(strict_types=1);
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 call_user_func(function () {
--- src/Plugin.php
+++ src/Plugin.php
@@ -2,17 +2,75 @@
 declare(strict_types=1);
 namespace SimplyYesNo;
 
+use TGM_Plugin_Activation;
+
 class Plugin
 {
-    public function bootstrap()
+    /**
+     * @var TGM_Plugin_Activation $tgmpa
+     */
+    private $tgmpa;
+
+    /**
+     * @var string $slug
+     */
+    private static $slug = 'simply-yes-no';
+
+    public function __construct()
     {
-        add_action('init', [$this, 'register_question_post_type']);
+        $this->tgmpa = TGM_Plugin_Activation::get_instance();
+        var_dump($this->tgmpa);
     }
 
-    public function register_question_post_type()
+    public function bootstrap()
     {
-        register_post_type('simply-yes-no', [
-            'label' => __('Questions', 'simply-yes-no'),
+        add_action('tgmpa_register', [$this, 'register_required_plugins']);
+        add_action('init', [$this, 'init']);
+    }
+
+    function init() {
+        if (!$this->tgmpa->is_tgmpa_complete()) {
+            return;
+        }
+
+        $this->registerPostType();
+    }
+
+    function register_required_plugins()
+    {
+        $plugins = [
+            [
+                'name' => 'Advanced Custom Fields',
+                'slug' => 'advanced-custom-fields',
+                'required' => true,
+            ]
+        ];
+
+        $config = [
+            'id' => 'tgmpa',
+            'dismissable' => false,
+            'is_automatic' => true,
+            'strings' => [
+                'notice_can_install_required' => _n_noop(
+                    'Simply Yes or No requires the following plugin: %1$s.',
+                    'Simply Yes or No requires the following plugins: %1$s.',
+                    self::$slug
+                ),
+                'notice_can_activate_required' => _n_noop(
+                    'Simply Yes or No requires following plugin active: %1$s.',
+                    'Simply Yes or No requires following plugins active: %1$s.',
+                    self::$slug
+                ),
+            ]
+        ];
+
+        tgmpa($plugins, $config);
+    }
+
+    private function registerPostType()
+    {
+        register_post_type(self::$slug, [
+            'label' => __('Questions', self::$slug),
             'public' => true,
             'exclude_from_search' => true,
             'publicly_queryable' => false,
